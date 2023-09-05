4fb41ce3599eab9ca3ffc1214d022ffe9f0ce0c6
rameez1822534 <ch_rameez89@hotmail.com>
2023-09-05 11:00:12 +0200

    def read dict added

--- pythondb.py
+++ pythondb.py
@@ -4,3 +4,11 @@ def db_connection():
     return psycopg2.connect(
         host="localhost"
 )
+def read_dict():
+dbconn = db_connection()
+cur = dbconn.cursor()
+cur.execute("SELECT id, word, translation FROM dictionary;")
+rows = cur.fetchall()
+cur.close()
+dbconn.close()
+return rows

ee829166bfe3ebe7dda9f7239ce3b68afee06a4e
rameez1822534 <ch_rameez89@hotmail.com>
2023-09-05 11:10:37 +0200

    git db conncection added

--- pythondb.py
+++ pythondb.py
@@ -1,11 +1,22 @@
 import psycopg2
 
 def db_connection():
-    return psycopg2.connect(
-        host="localhost"
-)
-def read_dict():
-dbconn = db_connection()
+    try:
+        # Attempt to establish a connection to the database
+        conn = psycopg2.connect(
+            host="localhost",
+            port="5432",  # Usually port number 5432 for PostgreSQL
+            database="phonedb",
+            user="postgres",
+            password="18@Rameez22"  # Replace with your own password
+        )
+        return conn
+    except Exception as e:
+        print(f"Database connection error: {e}")
+        return None
+
+def read_dict():    
+    dbconn = db_connection()
 cur = dbconn.cursor()
 cur.execute("SELECT id, word, translation FROM dictionary;")
 rows = cur.fetchall()
@@ -14,6 +25,6 @@ dbconn.close()
 return rows
 
 while True: ## REPL - Read Execute Program Loop
-cmd = input("Command: ")
+    cmd = input("Command: ")
 if cmd == "quit":
-exit()
+    exit()
